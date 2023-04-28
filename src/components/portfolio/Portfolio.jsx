8cbddba58e95f97df1051d53198efd5f6325ab0a
Amateo642 <kitlyaevdd@mail.com>
2023-04-28 18:29:02 +0300

    portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import "./portfolio.css";
 import IMG1 from '../../assets/portfolio 1.jpg';
+import IMG2 from '../../assets/portfolio 2.jpg';
+import { useTranslation } from 'react-i18next';
 
 const data = [
   {
@@ -10,28 +12,37 @@ const data = [
     github: 'https://github.com/Amateo642/K-pop.choice',
     demo: 'https://kpop-game.vercel.app/'
   },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Books search app using Google books api.',
+    github: 'https://github.com/Amateo642/Library',
+    demo: 'https://library-amateo642.vercel.app/'
+  }
 ]
 
 const Portfolio = () => {
+  const { t } = useTranslation();
+  const works = t('portfolio.works', { returnObjects: true });
   return (
     <section id='portfolio' className='section'>
-      <h5>My recent work</h5>
-      <h2>Portfolio</h2>
+      <h5>{t('portfolio.recent')}</h5>
+      <h2>{t('portfolio.header')}</h2>
 
       <div className="container portfolio__container">
         {
-          data.map(({id, image, title, github, demo}) => {
+          works.map(({title}, index) => {
             return (
-              <article key={id} className="portfolio__item">
+              <article key={data[index].id} className="portfolio__item">
                 <div className="portfolio__item-image">
-                  <img src={image} alt={title} />
+                  <img src={data[index].image} alt={data[index].title} />
                 </div>
                 <h3>
                   {title}
                 </h3>
                 <div className="portfolio__item-cta">
-                  <a href={github} className='btn' target='_blank'>Github</a>
-                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                  <a href={data[index].github} className='btn' target='_blank'>{t('portfolio.github')}</a>
+                  <a href={data[index].demo} className='btn btn-primary' target='_blank'>{t('portfolio.demo')}</a>
                 </div>
               </article>
             )
@@ -42,4 +53,4 @@ const Portfolio = () => {
   )
 }
 
-export default Portfolio
+export default Portfolio;
